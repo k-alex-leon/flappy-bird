690599aa93167788193be17336c698c0dcff9e37
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2025-01-10 22:49:53 -0500

    fix: game over bug

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -5,7 +5,7 @@ export default class Player extends Phaser.Physics.Arcade.Image {
     scene.physics.add.existing(this); // add to physics engine
     scene.events.on("update", this.update, this); // add to global update list
     this.setOrigin(0.5); // this is the midle of the image
-    this.setDisplaySize(80, 80)
+    this.setDisplaySize(80, 80);
     this.setCollideWorldBounds(true);
 
     this.cursors = scene.input.keyboard.createCursorKeys();
@@ -13,17 +13,16 @@ export default class Player extends Phaser.Physics.Arcade.Image {
 
   update(time, delta) {
     super.update(time, delta);
-
+    if (!this.scene) return;
     // checks if key is pressed and sets a delay
     if (this.scene.input.keyboard.checkDown(this.cursors.up, 450)) {
       this.setVelocityY(-100);
-      this.scene.sound.play('jump')
+      this.scene.sound.play("jump");
     }
 
     // changes body angle depending on y velocity
-    let angle = Math.atan2(this.body.velocity.y, 180)
+    let angle = Math.atan2(this.body.velocity.y, 180);
 
-    this.setRotation(angle)
-
+    this.setRotation(angle);
   }
 }
