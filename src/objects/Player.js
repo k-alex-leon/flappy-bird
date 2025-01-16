1cadf46a66deaf2338974e12c5bca733e63742b9
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2025-01-15 22:37:43 -0500

    refactor: performance upgrates

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -3,17 +3,19 @@ export default class Player extends Phaser.Physics.Arcade.Image {
     super(scene, x, y, texture);
     scene.add.existing(this); // add to display list
     scene.physics.add.existing(this); // add to physics engine
-    scene.events.on("update", this.update, this); // add to global update list
+    this.body.setCircle(140); // Sets a circle collision body with radius 16
+    this.body.setOffset(20, -10); // Adjust the circle a bit
     this.setOrigin(0.5); // this is the midle of the image
     this.setDisplaySize(80, 80);
     this.setCollideWorldBounds(true);
 
-    this.cursors = scene.input.keyboard.createCursorKeys();
+    this.setDepth(99)
+    this.cursors = scene.input.keyboard.createCursorKeys(); // input controlls
   }
 
   update(time, delta) {
     super.update(time, delta);
-    if (!this.scene) return;
+    if (!this.scene || !this.cursors) return;
     // checks if key is pressed and sets a delay
     if (this.scene.input.keyboard.checkDown(this.cursors.up, 450)) {
       this.setVelocityY(-100);
@@ -24,5 +26,22 @@ export default class Player extends Phaser.Physics.Arcade.Image {
     let angle = Math.atan2(this.body.velocity.y, 180);
 
     this.setRotation(angle);
+  }
+
+  failAnimation() {
+    this.cursors = null
+    this.setVelocity(0);
+    this.body.allowGravity = false;
+    this.setCollideWorldBounds(false);
+
+    this.scene.tweens.add({
+      targets: this,
+      angle: -765, // Rotate the player
+      duration: 1000,
+      ease: "Cubic.easeOut",
+    });
+
+    // Simulate a fall to the ground
+    this.setVelocityY(300);
   }
 }
