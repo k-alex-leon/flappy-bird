0d1f638b4df9cc8523a2640b5d278d676e1af236
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2025-01-16 21:17:14 -0500

    style: new font, image and website title

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -8,11 +8,15 @@ export default class PreloaderScene extends Phaser.Scene {
         this.load.image('player', './images/bird.PNG')
         this.load.image('tube', './images/pipeline.png')
         this.load.image('ground', './images/ground.jpg')
+        this.load.image('arrow', './images/uparrow.png')
         // audio resoruce
 
         this.load.audio('jump', './audio/jump.mp3')
         this.load.audio('fail', './audio/fail.mp3')
         this.load.audio('coin', './audio/coin.mp3')
+
+        // fonts
+        this.load.font('Jersey', './fonts/Jersey15-Regular.ttf', 'truetype')
     }
 
     create(){
